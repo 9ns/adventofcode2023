85bceca0134a67b3ab3d116bfd9fba2eccc09bbf
christian <christian.strom92@gmail.com>
2023-12-04 22:21:41 +0100

    why no work?

--- 1/removeletters.js
+++ 1/removeletters.js
@@ -1,6 +1,20 @@
 const fs = require("fs")
 const filePath = "./input.txt"
 
+const magic = (row) => {
+    return row
+        .replace("zero", "zero0zero")
+        .replace("one", "one1one")
+        .replace("two", "two2two")
+        .replace("three", "three3three")
+        .replace("four", "four4four")
+        .replace("five", "five5five")
+        .replace("six", "six6six")
+        .replace("seven", "seven7seven")
+        .replace("eight", "eight8eight")
+        .replace("nine", "nine9nine")
+}
+
 fs.readFile(filePath, "utf8", (error, data) => {
     if (error) {
         console.error(`Error reading file, error: ${error}`)
@@ -10,18 +24,18 @@ fs.readFile(filePath, "utf8", (error, data) => {
     const rows = data.split("\n")
 
     const total = rows.reduce((sum, row) => {
-        const noletters = row.replace(/[a-zA-Z]/g, "").trim()
+        const magicRow = magic(row.toLowerCase())
+        const noletters = magicRow.replace(/[a-zA-Z]/g, "").trim()
         let numbers = ""
 
         if (noletters.length > 1) {
             numbers = noletters.charAt(0) + "" + noletters.charAt(noletters.length - 1)
         } else {
-            numbers = noletters + "" + noletters
+            numbers = noletters.charAt(0) + "" + noletters.charAt(0)
         }
 
         return sum + Number(numbers)
     }, 0)
-
 
     console.log(total)
 })
